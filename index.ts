961aecbc8540d3b4111877d96f85716bc3b44899
alongkot kimhun <kim.alongkot@gmail.com>
2026-09-04 01:33:14 +0700

    my first leet 1. Two Sum

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
-function foo(x:Function):void{
-console.log("Foo")
-  x()
+function twoSum(nums: number[], target: number): number[] {
+    let serealnum:number[] = []
+
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length;j++){
+     if(Number(nums[i]) + Number(nums[j]) === Number(target)){
+        return serealnum = [i, j]
+        }  
+    }
 }
-foo(() => {})
+    return serealnum
+        
+}
+console.log(twoSum([2,7,11,15], 18
+))
